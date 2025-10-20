effd359dcfdc4d243e712cf55b0f4c3924129d04
Hyenghwan Kim <joey3798@naver.com>
2025-10-20 17:26:08 +0900

    일단 간단한 타일맵 하나 가지고 플레이어 움직임 기능 구현

---
+++ Assets/Script/ArrowMovemnt.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class ArrowMovemnt
+    : MonoBehaviour
+{
+    private enum Direction
+    {
+        Right = 0, 
+        Left = 1, 
+        Up = 2, 
+        Down = 3,
+    }
+
+    public float speed = 5.0f;
+
+    public KeyCode[] moveKeys =
+    {
+        KeyCode.RightArrow,
+        KeyCode.LeftArrow,
+        KeyCode.UpArrow,
+        KeyCode.DownArrow
+    };
+
+
+
+    void Update()
+    {
+        if (Input.GetKey(moveKeys[(int)Direction.Right]))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(moveKeys[(int)Direction.Left]))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(moveKeys[(int)Direction.Up]))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(moveKeys[(int)Direction.Down]))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+}
